200c691d72ec4395fde07580a8ca9dc11a87964e
Amanda Ng <amandaleahng@gmail.com>
2020-07-14 06:04:20 +0800

    trying to fix link js for verification

--- app/javascript/packs/link.js
+++ app/javascript/packs/link.js
@@ -1,11 +1,6 @@
-
 $("tr[data-link]").click(function() {
   $(document).on('page:change', function() {
     window.location = $(this).data("link")
   });
-})
-
-
-$("tr[data-link]").click(function() {
   window.location = $(this).data("link")
 })
