0a53fb755cf626f5146389b538e3278f279812e5
Amanda Ng <amandaleahng@gmail.com>
2020-07-13 20:12:28 +0800

    verification solution attempt

--- app/javascript/packs/link.js
+++ app/javascript/packs/link.js
@@ -1,3 +1,7 @@
+
 $("tr[data-link]").click(function() {
-  window.location = $(this).data("link")
+  $(document).on('page:change', function() {
+    window.location = $(this).data("link")
+  });
 })
+
