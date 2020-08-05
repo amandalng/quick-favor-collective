e4b116b7f328d2cb7db1f058c1c57b9a04be02f9
Amanda Ng <amandaleahng@gmail.com>
2020-08-06 05:57:23 +0800

    changed navbar buttons

--- app/controllers/favors_controller.rb
+++ app/controllers/favors_controller.rb
@@ -30,7 +30,7 @@ class FavorsController < ApplicationController
 
     if @favor.save
       FavorMailer.with(favor: @favor).send_favor.deliver_now
-      redirect_to confirmation_favor_path(@favor)
+      redirect_to favors_path
      else
       render 'new'
     end
