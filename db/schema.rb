82bed0c18faf8f7ffa026e9fb57462efd0fe1009
Amanda Ng <amandaleahng@gmail.com>
2020-08-14 05:44:03 +0800

    added website

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_08_204019) do
+ActiveRecord::Schema.define(version: 2020_08_13_213519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,7 @@ ActiveRecord::Schema.define(version: 2020_08_08_204019) do
     t.string "funfact"
     t.string "country"
     t.date "birthdate"
+    t.string "website"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
